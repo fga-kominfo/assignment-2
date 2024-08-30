e7c586877593897e478c5bcb9047414ab4f8767c
abdillahnurr <abdillahnurr12@gmail.com>
2024-08-30 18:14:53 +0800

    Modifikasi display form

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// Menangkap elemen-elemen yang diperlukan
-const nameInput = document.getElementById('name');
-const roleInput = document.getElementById('role');
-const availabilityInput = document.getElementById('availability');
-const ageInput = document.getElementById('age');
-const locationInput = document.getElementById('location');
-const experienceInput = document.getElementById('experience');
-const emailInput = document.getElementById('email');
+// menampilkan card form
+document.getElementById('editButton').addEventListener('click', function () {
+  const cardForm = document.getElementById('cardForm');
+  if (cardForm.style.display === 'none' || cardForm.style.display === '') {
+      cardForm.style.display = 'block';
+  } else {
+      cardForm.style.display = 'none';
+  }
+});
 
 const profileName = document.getElementById('nameValue');
 const profileRole = document.getElementById('roleValue');
@@ -21,13 +22,13 @@ const submitButton = document.getElementById('submitButton');
 // Menambahkan event listener untuk tombol submit
 submitButton.addEventListener('click', function () {
   // Mengambil nilai dari input form
-  const nameValue = nameInput.value;
-  const roleValue = roleInput.value;
-  const availabilityValue = availabilityInput.value;
-  const ageValue = ageInput.value;
-  const locationValue = locationInput.value;
-  const experienceValue = experienceInput.value;
-  const emailValue = emailInput.value;
+  const nameValue = document.getElementById('name').value;
+  const roleValue = document.getElementById('role').value;
+  const availabilityValue = document.getElementById('availability').value;
+  const ageValue = document.getElementById('age').value;
+  const locationValue = document.getElementById('location').value;
+  const experienceValue = document.getElementById('experience').value;
+  const emailValue = document.getElementById('email').value;
 
   // Menampilkan data pada Profile Section
   profileName.textContent = nameValue;
